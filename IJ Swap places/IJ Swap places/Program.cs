0828b481c67ca9b8f3b9ba3692dfe8c09843da4f
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-12 12:34:11 +0500

    init

---
+++ IJ Swap places/IJ Swap places/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IJ_Swap_places
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jordan";
+            string surname = "Maichel";
+            string swap;
+            Console.WriteLine($"{name} {surname}");
+            swap = name;
+            name = surname;
+            surname = swap;
+            Console.WriteLine($"{name} {surname}");
+        }
+    }
+}
